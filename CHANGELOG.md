d9e62fe648479d16dd9fd078f01edc29091d269b
Morasiu <moras2601@wp.pl>
2020-09-22 10:40:22 +0200

    Version 1.4.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.4.2] - 2020-09-22
+### Fixed
+ - Now invalid json returns null instead of throwing JsonParser exception and causing HTTP 500
+
 ## [1.4.1] - 2020-06-20
 ### Changed
  - Updates dendencies to AspNetCore 3.1
--- src/Swashbuckle.AspNetCore.JsonMultipartFormDataSupport/JsonModelBinder.cs
+++ src/Swashbuckle.AspNetCore.JsonMultipartFormDataSupport/JsonModelBinder.cs
@@ -14,7 +14,7 @@ namespace Swashbuckle.AspNetCore.JsonMultipartFormDataSupport {
 		private readonly IOptions<JsonOptions> _jsonOptions;
 		private readonly IOptions<MvcNewtonsoftJsonOptions> _newtonsoftJsonOptions;
 
-		public JsonModelBinder(){}
+		public JsonModelBinder() { }
 
 		public JsonModelBinder(IOptions<JsonOptions> jsonOptions) {
 			_jsonOptions = jsonOptions;
@@ -23,10 +23,19 @@ namespace Swashbuckle.AspNetCore.JsonMultipartFormDataSupport {
 		public JsonModelBinder(IOptions<MvcNewtonsoftJsonOptions> newtonsoftJsonOptions) {
 			_newtonsoftJsonOptions = newtonsoftJsonOptions;
 		}
+
 		/// <inheritdoc />
 		public Task BindModelAsync(ModelBindingContext bindingContext) {
 			if (bindingContext == null) {
 				throw new ArgumentNullException(nameof(bindingContext));
+			}
+
+			string modelBindingKey;
+			if (bindingContext.IsTopLevelObject) {
+				modelBindingKey = bindingContext.BinderModelName ?? string.Empty;
+			}
+			else {
+				modelBindingKey = bindingContext.ModelName;
 			}
 
 			// Check the value sent in
@@ -38,13 +47,36 @@ namespace Swashbuckle.AspNetCore.JsonMultipartFormDataSupport {
 				var valueAsString = valueProviderResult.FirstValue;
 
 				object result;
-				if (_jsonOptions != null)
-					result = JsonSerializer.Deserialize(valueAsString, bindingContext.ModelType, _jsonOptions.Value.JsonSerializerOptions);
-				else if (_newtonsoftJsonOptions != null)
-					result = JsonConvert.DeserializeObject(valueAsString, bindingContext.ModelType,
-						_newtonsoftJsonOptions.Value.SerializerSettings);
-				else
-					result = JsonSerializer.Deserialize(valueAsString, bindingContext.ModelType);
+				if (_jsonOptions != null) {
+					try {
+						result = JsonSerializer.Deserialize(valueAsString, bindingContext.ModelType,
+							_jsonOptions.Value.JsonSerializerOptions);
+					}
+					catch (JsonException e) {
+						bindingContext.ModelState.AddModelError(modelBindingKey, e, bindingContext.ModelMetadata);
+						return Task.CompletedTask;
+					}
+				}
+				else if (_newtonsoftJsonOptions != null) {
+					try {
+						result = JsonConvert.DeserializeObject(valueAsString, bindingContext.ModelType,
+							_newtonsoftJsonOptions.Value.SerializerSettings);
+					}
+					catch (JsonReaderException e) {
+						bindingContext.ModelState.TryAddModelException(modelBindingKey, e);
+						return Task.CompletedTask;
+					}
+				}
+				else {
+					try {
+						result = JsonSerializer.Deserialize(valueAsString, bindingContext.ModelType);
+					}
+					catch (JsonException e) {
+						bindingContext.ModelState.TryAddModelException(modelBindingKey, e);
+						return Task.CompletedTask;
+					}
+				}
+
 				if (result != null) {
 					bindingContext.Result = ModelBindingResult.Success(result);
 					return Task.CompletedTask;
