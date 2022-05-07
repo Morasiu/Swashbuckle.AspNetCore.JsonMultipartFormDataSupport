a5bc70cd3107a6fce3511e7fc08956b45ed434d4
Morasiu <moras2601@wp.pl>
2022-05-07 12:09:19 +0200

    Required example

--- src/Demo/Controllers/ProductController.cs
+++ src/Demo/Controllers/ProductController.cs
@@ -15,6 +15,20 @@ namespace Demo.Controllers {
 			var image = data.File;
 			return Ok(new {json, image?.FileName});
 		}
+		
+		[HttpPost("required")]
+		public IActionResult Post([FromForm] MultipartRequiredFormData<Product> data) {
+			var json = data.Json ?? throw new NullReferenceException(nameof(data));
+			var image = data.File;
+			return Ok(new {json, image?.FileName});
+		}
+		
+		[HttpPost("required/wrapper")]
+		public IActionResult Post([FromForm] RequiredProductWrapper wrapper) {
+			var wrapperProduct = wrapper.Product ?? throw new NullReferenceException(nameof(wrapper.Product));
+			var images = wrapper.Files;
+			return Ok(new {wrapperProduct, images = images?.Select(a => a.FileName)});
+		}
 
 		[HttpPost("wrapper")]
 		public IActionResult PostWrapper([FromForm] ProductWrapper wrapper) {
---
+++ src/Demo/Models/RequiredProductWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+using Swashbuckle.AspNetCore.JsonMultipartFormDataSupport.Attributes;
+
+namespace Demo.Models {
+	public class RequiredProductWrapper {
+		[Required]
+		[FromJson] // <-- This attribute is required for binding.
+		public Product Product { get; set; }
+		
+		[Required]
+		public IFormFileCollection  Files { get; set; }
+	}
+}
