defdbec5baedf17184f06901d6a4e00e20dedf79
Morasiu <morasiu2@gmail.com>
2022-05-29 12:19:01 +0200

    FluentValidationInSwagger

--- src/Demo/Startup.cs
+++ src/Demo/Startup.cs
@@ -1,6 +1,7 @@
 using Demo.Models;
 using Demo.Models.Products;
 using FluentValidation.AspNetCore;
+using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -46,6 +47,7 @@ namespace Demo {
 					Version = "v1"
 				});
 			});
+			services.AddFluentValidationRulesToSwagger();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
